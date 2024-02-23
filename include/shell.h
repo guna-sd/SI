23bc45db679eac56547b55dc9d8de618ca9bf23c
guna-sd <119688848+guna-sd@users.noreply.github.com>
2024-02-23 13:37:38 +0530

    Update shell.h

--- include/shell.h
+++ include/shell.h
@@ -1,9 +1,10 @@
-#ifndef _SHELL_H_
+#if !defined (_SHELL_H_)
 #define _SHELL_H_
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
 #include <termios.h>
 #include <string.h>
 #include <sys/types.h>
@@ -18,9 +19,7 @@
 #include <sys/mman.h>
 
 #define CMD_SUCCESS 0
-#define CMD_FAILURE 1
 #define CMD_ERROR -1
-#define CMD_MAX_SIZE 1024
 
 #define _EXEC    0 
 #define OR_EXEC  1  // ---> EXECUTION OF COMMAND USING LOGICAL OR '||'
@@ -29,29 +28,79 @@
 #define REDIRECT_EXEC 4 // ---> EXECUTION OF COMMAND FOLLOWED BY '>'
 #define PIPE_EXEC 5  // ---> EXECUTION OF COMMAND FOLLOWED BY '|'
 
-static pid_t SHELL_PGID;
-static int shell_is_interactive;
-struct termios shell_tmodes;
+#define BGREEN "\e[1;92m"
+#define BRED "\e[1;91m"
+#define BYELLOW "\e[1;93m"
+#define BBLUE "\e[1;94m"
+#define BMAGENTA "\e[1;95m"
+#define BCYAN "\e[1;96m"
+#define BWHITE "\e[1;97m"
+#define RESET "\e[0m"
+
+#define SI_VERSION "0.1"
+#define HIST_FILE	".si_history"
+#define HIST_MAX_Lines 1024
+#define MAX_ALIAS '100'
+#define MAX_CMD_SIZE 1024
+
+
+typedef struct {
+    char *cmd;
+    char *alias;
+    char *path;
+} Alias;
+
+typedef struct process
+{
+  struct process *next;
+  char **argv;
+  pid_t pid;
+  char completed;
+  char stopped;
+  int status;
+} process;
 
 
 // shell utils
-int init();
-int _fork(char *args);
-int _exec(char *args);
-int _exec_or(char *args);
-int _exec_and(char *args);
-int _exec_seq(char *args);
+void init();
+int _fork(char *argv[]);
+int _exec(char *argv[]);
+int _exec_or(char *argv[]);
+int _exec_and(char *argv[]);
+int _exec_seq(char *argv[]);
+int _exec_redirect(char *args, int mode, char *filename);
+int _exec_pipe(char *args);
+void _perror(char *ErrorMessage);
+void signalHandler_child();
+void signalHandler_interrupt();
 
 // cmd utils
 int _cd(char *argv[]);
 int _alias(char *argv[]);
-int _echo(char *argv[]);
-int _clear();
 int _version();
-int _quit();
 int _help();
 int _history();
 
+// shell utils
+void initial_screen();
+int _add_alias(char *args, char *alias);
+int _remove_alias(char *alias);
+void prompt();
+char* _input();
+
+// llm utils
+long time_in_ms();
+void safe_printf(char *piece);
+void matmul(float* xout, float* x, float* w, int n, int d);
+void softmax(float* x, int size);
+void rmsnorm(float* o, float* x, float* weight, int size);
+int compare_tokens(const void *a, const void *b);
+int compare(const void *a, const void *b);
+int sample_argmax(float *prob, int n);
+int sample_mult(float *prob, int n, float coin);
+unsigned int random_u32(unsigned long long *state);
+float random_f32(unsigned long long *state);
+void *read_file(char *filename);
 
 
 #endif
